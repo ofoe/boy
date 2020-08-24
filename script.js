a737e2bc113fa1a426efca4b0d9343c350d50ac5
Tyris <35848273+ofoe@users.noreply.github.com>
2020-08-24 05:21:23 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -79,6 +79,19 @@ function showSlides(n) {
 
 
 
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "300px";
+  document.getElementById("mySidenav").style.boxShadow = "0 0 0 10000px rgba(0,0,0,0.5)";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.boxShadow = "none";
+}
+
+
 
 
 
